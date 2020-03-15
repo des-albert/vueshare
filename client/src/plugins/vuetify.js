5d49bc7ac57e7e06d16348cdf65fea0e44a7d0e0
Des Albert <des.albert@gmail.com>
2020-03-14 19:58:00 -0700

    First Commit

---
+++ client/src/plugins/vuetify.js
@@ -0,0 +1,20 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib';
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                primary: "#3B125F",
+                secondary: "#8B5FBF",
+                accent: "#BF653F",
+                error: "#722530",
+                warning: "#A37513",
+                info: "#396893",
+                success: "#4CAF50"
+            }
+        }
+    }
+});
